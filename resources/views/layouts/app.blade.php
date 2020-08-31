bdc6492afd3646f5cc2bab3ad2af683ecbb82ea2
Dusan Vostic <vosticdusan@gmail.com>
2020-08-31 18:31:35 +0200

    Add bootstrap and import it in layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     <title>@yield('title')</title>
 </head>
 <body>
-    @yield('content') 
-    <!-- @section('content') 
-    @show -->
+    <div class="container">
+        @yield('content') 
+    </div>
 </body>
 </html>
